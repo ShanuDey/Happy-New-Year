f8abfa6642110604834031fb6e6ad10cce98d564
ShanuDey <sd4shanudey@gmail.com>
2020-01-01 14:56:14 +0530

    Make new wish from the user input name
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ if (name != 'null' && name != '') {
     console.log("This is not null");
     document.getElementById("wish").innerHTML = "From " + name;
 }
+
+const baseUrl = "file:///home/shanu/Documents/HappyNewYear/index.html?from=";
+
+function createWish() {
+    var value = document.getElementById("name").value;
+    document.getElementById("liveUrl").innerHTML = baseUrl + value;
+}
